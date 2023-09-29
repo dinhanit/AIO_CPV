25b7a80caf7004859145c5a11bd20fa101434402
dinhanit <ngodinhan2k3@gmail.com>
2023-09-29 20:05:26 +0700

    updateModel

--- AllModel.py
+++ AllModel.py
@@ -1,4 +1,6 @@
 import torch.nn as nn
+import torch
+#______________Day1_________________
 class SimpleCNN1(nn.Module):
     def __init__(self, num_classes = 10):
         super(SimpleCNN1, self).__init__()
@@ -46,7 +48,7 @@ class SimpleCNN2(nn.Module):
         x = nn.functional.relu(x)
         x = self.fc2(x)
         return x
-    
+#______________Day2_________________
 class CustomCNN(nn.Module):
     def __init__(self, num_classes = 10):
         super(CustomCNN, self).__init__()
@@ -89,40 +91,7 @@ class CustomCNN(nn.Module):
         x = self.sq_classifier(x)
         return x
 
-class SimpleCNN1Conv(nn.Module):
-    def __init__(self, num_classes=10):
-        super(SimpleCNN1Conv, self).__init__()
-        self.conv1 = nn.Sequential(
-            nn.Conv2d(3, 16, 3),
-            nn.BatchNorm2d(16),
-            nn.ReLU(inplace=True)
 
-        )
-        self.pool1 = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(16, 32, 3)
-        self.batchnorm2 = nn.BatchNorm2d(32)
-        self.pool2 = nn.MaxPool2d(2, 2)
-        self.avgpool = nn.AdaptiveAvgPool2d((16, 16))
-        self.fc1 = nn.Linear(32 * 16 * 16, 256)
-        self.fc2 = nn.Linear(256, num_classes)
-
-    def forward(self, x):
-        x = self.conv1(x)
-        x = self.pool1(x)
-        x = self.conv2(x)
-        x = self.batchnorm2(x)
-        x = nn.functional.relu(x)
-        x = self.pool2(x)
-        x = self.avgpool(x)
-        x = x.view(x.size(0), -1)
-        x = self.fc1(x)
-        x = nn.functional.relu(x)
-        x = nn.Dropout(p=0.2)(x)
-
-        x = self.fc2(x)
-        return x
-
-import torch.nn as nn
 
 class VGG16(nn.Module):
     def __init__(self, num_classes=10):
@@ -246,7 +215,6 @@ class VGG19(nn.Module):
         x = self.classifier(x)
         return x
 
-
 class BasicBlock(nn.Module):
     expansion = 1
 
@@ -320,4 +288,179 @@ class ResNet50(nn.Module):
         x = x.view(x.size(0), -1)
         x = self.fc(x)
         return x
+#_____________Day3________________
+class InvertedResidual(nn.Module):
+    def __init__(self, in_channels, out_channels, stride, expand_ratio):
+        super(InvertedResidual, self).__init__()
+        self.use_residual = stride == 1 and in_channels == out_channels
+        hidden_dim = in_channels * expand_ratio
 
+        layers = []
+        if expand_ratio != 1:
+            layers.append(nn.Conv2d(in_channels, hidden_dim, kernel_size=1, bias=False))
+            layers.append(nn.BatchNorm2d(hidden_dim))
+            layers.append(nn.ReLU6(inplace=True))
+        layers.extend([
+            nn.Conv2d(hidden_dim, hidden_dim, kernel_size=3, stride=stride, padding=1, groups=hidden_dim, bias=False),
+            nn.BatchNorm2d(hidden_dim),
+            nn.ReLU6(inplace=True),
+            nn.Conv2d(hidden_dim, out_channels, kernel_size=1, bias=False),
+            nn.BatchNorm2d(out_channels),
+        ])
+        self.conv = nn.Sequential(*layers)
+
+    def forward(self, x):
+        if self.use_residual:
+            return x + self.conv(x)
+        else:
+            return self.conv(x)
+
+class MobileNetV2(nn.Module):
+    def __init__(self, num_classes=10, width_mult=1.0, inverted_residual_setting=None):
+        super(MobileNetV2, self).__init__()
+        input_channel = 32
+        last_channel = 1280
+
+        if inverted_residual_setting is None:
+            inverted_residual_setting = [
+                # t, c, n, s
+                [1, 16, 1, 1],
+                [6, 24, 2, 2],
+                [6, 32, 3, 2],
+                [6, 64, 4, 2],
+                [6, 96, 3, 1],
+                [6, 160, 3, 2],
+                [6, 320, 1, 1],
+            ]
+
+        # Building first layer
+        input_channel = int(input_channel * width_mult)
+        self.last_channel = int(last_channel * width_mult)
+        self.features = [nn.Sequential(
+            nn.Conv2d(3, input_channel, kernel_size=3, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(input_channel),
+            nn.ReLU6(inplace=True)
+        )]
+        # Building inverted residual blocks
+        for t, c, n, s in inverted_residual_setting:
+            output_channel = int(c * width_mult)
+            for i in range(n):
+                if i == 0:
+                    self.features.append(InvertedResidual(input_channel, output_channel, s, t))
+                else:
+                    self.features.append(InvertedResidual(input_channel, output_channel, 1, t))
+                input_channel = output_channel
+        # Building last several layers
+        self.features.append(nn.Sequential(
+            nn.Conv2d(input_channel, self.last_channel, kernel_size=1, bias=False),
+            nn.BatchNorm2d(self.last_channel),
+            nn.ReLU6(inplace=True)
+        ))
+        # Make it a nn.Module
+        self.features = nn.Sequential(*self.features)
+
+        # Classifier
+        self.classifier = nn.Sequential(
+            nn.Dropout(0.2),
+            nn.Linear(self.last_channel, num_classes)
+        )
+
+    def forward(self, x):
+        x = self.features(x)
+        x = x.mean(3).mean(2)
+        x = self.classifier(x)
+        return x
+
+class InceptionModule(nn.Module):
+    def __init__(self, in_channels, out1x1, reduce3x3, out3x3, reduce5x5, out5x5, out1x1pool):
+        super(InceptionModule, self).__init__()
+
+        # 1x1 convolution branch
+        self.branch1x1 = nn.Sequential(
+            nn.Conv2d(in_channels, out1x1, kernel_size=1),
+            nn.ReLU(inplace=True)
+        )
+
+        # 1x1 convolution followed by 3x3 convolution branch
+        self.branch3x3 = nn.Sequential(
+            nn.Conv2d(in_channels, reduce3x3, kernel_size=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(reduce3x3, out3x3, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True)
+        )
+
+        # 1x1 convolution followed by 5x5 convolution branch
+        self.branch5x5 = nn.Sequential(
+            nn.Conv2d(in_channels, reduce5x5, kernel_size=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(reduce5x5, out5x5, kernel_size=5, padding=2),
+            nn.ReLU(inplace=True)
+        )
+
+        # 3x3 max pooling followed by 1x1 convolution branch
+        self.branch1x1pool = nn.Sequential(
+            nn.MaxPool2d(kernel_size=3, stride=1, padding=1),
+            nn.Conv2d(in_channels, out1x1pool, kernel_size=1),
+            nn.ReLU(inplace=True)
+        )
+
+    def forward(self, x):
+        branch1x1 = self.branch1x1(x)
+        branch3x3 = self.branch3x3(x)
+        branch5x5 = self.branch5x5(x)
+        branch1x1pool = self.branch1x1pool(x)
+
+        outputs = [branch1x1, branch3x3, branch5x5, branch1x1pool]
+        return torch.cat(outputs, 1)
+
+class GoogleNet(nn.Module):
+    def __init__(self, num_classes=10):
+        super(GoogleNet, self).__init__()
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3)
+        self.maxpool1 = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        self.conv2 = nn.Conv2d(64, 64, kernel_size=1)
+        self.conv3 = nn.Conv2d(64, 192, kernel_size=3, padding=1)
+        self.maxpool2 = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+
+        self.inception3a = InceptionModule(192, 64, 96, 128, 16, 32, 32)
+        self.inception3b = InceptionModule(256, 128, 128, 192, 32, 96, 64)
+
+        self.maxpool3 = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+
+        self.inception4a = InceptionModule(480, 192, 96, 208, 16, 48, 64)
+        self.inception4b = InceptionModule(512, 160, 112, 224, 24, 64, 64)
+        self.inception4c = InceptionModule(512, 128, 128, 256, 24, 64, 64)
+        self.inception4d = InceptionModule(512, 112, 144, 288, 32, 64, 64)
+        self.inception4e = InceptionModule(528, 256, 160, 320, 32, 128, 128)
+
+        self.maxpool4 = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+
+        self.inception5a = InceptionModule(832, 256, 160, 320, 32, 128, 128)
+        self.inception5b = InceptionModule(832, 384, 192, 384, 48, 128, 128)
+
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        self.dropout = nn.Dropout(0.4)
+        self.fc = nn.Linear(1024, num_classes)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.maxpool1(x)
+        x = self.conv2(x)
+        x = self.conv3(x)
+        x = self.maxpool2(x)
+        x = self.inception3a(x)
+        x = self.inception3b(x)
+        x = self.maxpool3(x)
+        x = self.inception4a(x)
+        x = self.inception4b(x)
+        x = self.inception4c(x)
+        x = self.inception4d(x)
+        x = self.inception4e(x)
+        x = self.maxpool4(x)
+        x = self.inception5a(x)
+        x = self.inception5b(x)
+        x = self.avgpool(x)
+        x = x.view(x.size(0), -1)
+        x = self.dropout(x)
+        x = self.fc(x)
+        return x
